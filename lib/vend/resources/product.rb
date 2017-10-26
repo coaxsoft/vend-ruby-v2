a46934d1666ed765ec3ff3f85b84523a8de4f0bf
Yurkiv Misha <m.yurkiv@coaxsoft.com>
2017-10-26 11:34:08 +0300

    Add update and find v0.9 action for product

--- lib/vend/resources/product.rb
+++ lib/vend/resources/product.rb
@@ -5,6 +5,14 @@ module Vend
   class Product < Resource
     include Vend::ResourceActions.new uri: 'products'
 
+    def self.find_v0_9(resource_id, params = {})
+      get "products/#{resource_id}", params
+    end
+
+    def self.update(params = {})
+      post 'products', params
+    end
+
     def self.inventory(resource_id, params = {})
       get "2.0/products/#{resource_id}/inventory", params
     end
