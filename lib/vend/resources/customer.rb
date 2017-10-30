4da060907cc113473791a50f24f9aa64a2582771
Yurkiv Misha <m.yurkiv@coaxsoft.com>
2017-10-30 14:38:33 +0200

    Add all customers v0_9 action

--- lib/vend/resources/customer.rb
+++ lib/vend/resources/customer.rb
@@ -4,5 +4,9 @@
 module Vend
   class Customer < Resource
     include Vend::ResourceActions.new uri: 'customers'
+
+    def self.all_v0_9(params = {})
+      get 'customers', params
+    end
   end
 end
