bc42ac2e9bd48a2e1569c9240df13706ae6f6c76
Yurkiv Misha <m.yurkiv@coaxsoft.com>
2017-11-23 12:37:54 +0200

    Add returning feature for Sale

--- lib/vend/resources/sale.rb
+++ lib/vend/resources/sale.rb
@@ -8,5 +8,17 @@ module Vend
     def self.create(params = {})
       post 'register_sales', params
     end
+
+    def self.return(resource_id, params = {})
+      put "/api/2.0/sales/#{resource_id}/actions/return", params
+    end
+
+    def self.register_sale(resource_id, params = {})
+      get "/api/register_sales/#{resource_id}", params
+    end
+
+    def self.finalize_return(params = {})
+      post '/api/register_sales', params
+    end
   end
 end
